bf624716e90d01aea14ae6d293a814bf6c6fd4d4
Sarai6e <1472173@senati.pe>
2024-04-03 13:21:35 -0500

    MyPrimeApp

---
+++ 3_04_24/MyPrimerApp/MyPrimerApp/MyPrimerApp.cs.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MyPrimerApp
+{
+    public partial class MyPrimerApp : ContentPage
+    {
+        public MyPrimerApp()
+        {
+            InitializeComponent();
+        }
+    }
+}
